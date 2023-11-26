3e8c8221ea257db79709955311deb14aa0992e65
Thomas Briot <thomas.briot82@gmail.com>
2023-11-26 15:12:59 -0500

    test: remove one unit test

--- calculator_test.go
+++ calculator_test.go
@@ -2,29 +2,30 @@ package main
 
 import "testing"
 
-func TestAdd(t *testing.T) {
-	tables := []struct {
-		x int
-		y int
-		n int
-		e string
-	}{
-		{1, 2, 3, ""},
-		{1.0, 2.0, 3.0, ""},
-		{0, 2.0, 2.0, ""},
-		{2.0, 0, 2.0, ""},
-		{-4, 2.0, -2.0, ""},
-	}
+/*
+	func TestAdd(t *testing.T) {
+		tables := []struct {
+			x int
+			y int
+			n int
+			e string
+		}{
+			{1, 2, 3, ""},
+			{1.0, 2.0, 3.0, ""},
+			{0, 2.0, 2.0, ""},
+			{2.0, 0, 2.0, ""},
+			{-4, 2.0, -2.0, ""},
+		}
 
-	for _, table := range tables {
-		if total, err := Add(table.x, table.y); err != nil && err.Error() != table.e {
-			t.Errorf("Add of (%d+%d) produced wrong error, got %v, want %v.", table.x, table.y, err.Error(), table.e)
-		} else if total != table.n {
-			t.Errorf("Add of (%d+%d) produced wrong result, got %d, want %d.", table.x, table.y, total, table.n)
+		for _, table := range tables {
+			if total, err := Add(table.x, table.y); err != nil && err.Error() != table.e {
+				t.Errorf("Add of (%d+%d) produced wrong error, got %v, want %v.", table.x, table.y, err.Error(), table.e)
+			} else if total != table.n {
+				t.Errorf("Add of (%d+%d) produced wrong result, got %d, want %d.", table.x, table.y, total, table.n)
+			}
 		}
 	}
-}
-
+*/
 func TestSubtract(t *testing.T) {
 	tables := []struct {
 		x int
